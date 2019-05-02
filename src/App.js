0e0f370ade5b3042e1fbd33442bcc5c64dd6c55a
Jolt64 <jonathantelford64@gmail.com>
2019-05-02 10:51:52 -0600

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="menuText">Start Bootstrap</div>
+          <div className="menuButton">Menu &#9776;</div>
+          <div className="menuBar">
+              <button>SERVICES</button>
+              <button>PORTFOLIO</button>
+              <button>ABOUT</button>
+              <button>CONTACT</button>
+          </div>
         </header>
+        <section className="landingSection">
+          <div className="greetingBlock">
+            <p className="welcomeSpan">Welcome To Our Studio!</p>
+            <p className="greetingSpan">IT'S NICE TO MEET YOU</p>
+            <div className="tellMeMoreButton">Tell Me More</div>
+          </div>
+        </section>
       </div>
     );
   }
